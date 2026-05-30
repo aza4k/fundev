23e319fd162129f46c9a54f29d28bd527526b2a0
Azamat <aza4kf@gmail.com>
2026-05-30 18:52:52 +0500

    Configured volume

--- studio/settings.py
+++ studio/settings.py
@@ -94,8 +94,8 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', BASE_DIR / 'media')
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 

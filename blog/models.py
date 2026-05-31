62b151ff177186ab7f0b043c650475d8b9898835
Azamat <aza4kf@gmail.com>
2026-05-31 12:18:46 +0500

    Configured sitemap files

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
@@ -11,12 +12,17 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    seo_title = models.CharField(max_length=255, blank=True, null=True, help_text="Optional SEO Title to override the default title")
+    meta_description = models.TextField(blank=True, null=True, help_text="Optional Meta Description for SEO purposes")
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'slug': self.slug})
 
     def save(self, *args, **kwargs):
         if not self.slug:

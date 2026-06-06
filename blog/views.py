ac7b994001fdba9bf42ea6db0ef8bf9b9937fbb3
Azamat <aza4kf@gmail.com>
2026-06-06 16:08:36 +0500

    Seo optimized

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
+from django.http import HttpResponse
 from .models import Post
 
 def index(request):
@@ -18,3 +19,10 @@ def all_posts(request):
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug, is_published=True)
     return render(request, 'blog_detail.html', {'post': post})
+
+def robots_txt(request):
+    content = """User-agent: *
+Allow: /
+Sitemap: https://fundev.uz/sitemap.xml
+"""
+    return HttpResponse(content, content_type="text/plain")
--- studio/urls.py
+++ studio/urls.py
@@ -3,7 +3,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.views.generic import RedirectView
 from blog.sitemaps import PostSitemap, StaticViewSitemap
+from blog.views import robots_txt
 
 sitemaps = {
     'posts': PostSitemap,
@@ -14,6 +16,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('tinymce/', include('tinymce.urls')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', robots_txt),
+    path('favicon.ico', RedirectView.as_view(url='/static/images/logo-og.png')),
     path('', include('blog.urls')),
 ]
 
